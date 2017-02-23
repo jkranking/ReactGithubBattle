28f7db7ffc99246cfec5fa6ef18f4aefd62f0659
Jeff Kranking <jkranking@gmail.com>
2017-02-23 13:35:06 -0800

    adds main and home components

--- app/index.js
+++ app/index.js
@@ -1,12 +1,24 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var routes = require('./config/routes');
+// var Raven = require('raven-js')
 
-var Hello = React.createClass({
-  render: function () {
-    return (
-      <div>Hello ReactJS Program!</div>
-    )
-  }
-});
+// Get your keys by signing up for Sentry at sentry.reactjsprogram.com
+// var sentryKey = 'YOUR_SENTRY_KEY'
+// var sentryApp = 'YOUR_SENTRY_AP_ID'
+// var sentryURL = 'https://' + sentryKey + '@app.getsentry.com/' + sentryApp
 
-ReactDOM.render(<Hello />, document.getElementById('app'));
+// var _APP_INFO = {
+//   name: 'Github Battle',
+//   branch: 'video4',
+//   version: '1.0'
+// }
+
+// Raven.config(sentryURL, {
+//   release: _APP_INFO.version,
+//   tags: {
+//     branch: _APP_INFO.branch,
+//   }
+// }).install()
+
+ReactDOM.render(routes, document.getElementById('app'));
